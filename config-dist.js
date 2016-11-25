fef2307b46d65eda9699f13c3f38e3b04fc5c528
Daithi Coombes <webeire@gmail.com>
2016-11-25 02:28:06 +0000

    configs

---
+++ config-dist.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * Fill out params below and save as `config.js`
+ *
+ * @link http://docs.api.site.tld/register-app
+ * @author me <me@me.me>
+ */
+
+const config = {
+
+    client_id: "",
+    client_secret: "",
+    app_name: "",
+}
+
+module.exports = config;
